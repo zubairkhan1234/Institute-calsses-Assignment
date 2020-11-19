dbd891524510d0f367c92e880119ad60e2cf57d0
Muhammad Zubair <zubairsaleem@gmail.com>
2020-11-18 21:33:34 -0800

    class 01

---
+++ class 01/app.js
@@ -0,0 +1,21 @@
+// chapter 01 start 
+
+
+alert("hello world")
+
+
+// chaper 01 end 
+
+// chapter start 02
+
+
+var name = "mark";
+
+window.alert(name)
+
+
+var name = "zubair";
+
+window.alert(name)
+
+// chapter 02 end 
