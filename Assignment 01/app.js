2176fb8d1fa8a7dd28daf74de0fcaa8e256f5b64
Muhammad Zubair <zubairsaleem@gmail.com>
2020-11-22 13:22:03 -0800

    23-11-2020

---
+++ Assignment 01/app.js
@@ -0,0 +1,20 @@
+
+
+
+// function abc 
+
+// let a = document.getElementById("numberOne")
+// let b = document.getElementById("numberTwo")
+   
+//     c = ( a + b)
+
+    function abc() {
+
+        let number1 = document.getElementById("number1").value
+        let number2 = document.getElementById("number2").value
+
+        number1 = parseInt(number1)
+        number2 = parseInt(number2)
+
+        console.log(number1 + number2);
+    }
